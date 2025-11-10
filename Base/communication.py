92f0048191c2deda3490d1b633ea25f5b1fdd14f
Alejandro Velázquez Tundidor <156972029+LeAle4@users.noreply.github.com>
2025-11-10 16:09:41 -0300

    Sent Packet to data.py file

--- Base/communication.py
+++ Base/communication.py
@@ -1,6 +1,7 @@
 import serial
 import serial.tools.list_ports
 
+from Base.data import Packet
 from typing import Generator
 
 HEAD = b"\x14"
@@ -9,75 +10,6 @@ IMAGE_TYPE = b"\x00"
 TYPES_PAYLOAD = {
     IMAGE_TYPE: 48,
 }
-
-class Packet:
-    """Handles packet data using the PHUC Protocol.
-    
-    Header
-    ------
-    1 byte: Magic Num (0x69)
-    1 byte: Packet type
-
-    Body
-    ----
-    4 bytes: timestamp
-    Up to 48 bytes: data
-
-    Tail
-    ----
-    2 bytes: CRC data
-    
-    Packet size (8 -> 56) inclusive
-
-    Parameters
-    ----------
-
-    Methods
-    -------
-
-    """
-    def __init__(self, head:bytes, type:bytes, timestamp:bytes, data:bytes, crc:bytes, verbose:bool = False):
-        self.head = head
-        self.type = type
-        self.timestamp = timestamp
-        self.data = data
-        self.crc = crc
-        self.crcpass = self.crc_check(self.head + self.type + self.timestamp + self.data)
-        self.full = self.head + self.type + self.timestamp + self.data + self.crc
-
-        if verbose:
-            if not self.crcpass:
-                print(f"Packet of type {self.type} at {self.timestamp} failed CRC.")
-
-
-    def asjson(self)->dict:
-        """Returns a dictionary containing all the packet information to save in a json file."""
-        return {"Type": self.type, "Timestamp": self.timestamp, "Data": self.data, "CRCPass": self.crcpass}
-
-    @staticmethod
-    def crc_check(data: bytes) -> bool:
-        """Checks if the CRC is correct."""
-        crc = 0xFFFF
-        for b in data:
-            crc ^= b << 8
-            for _ in range(8):
-                if crc & 0x8000:
-                    crc = (crc << 1) ^ 0x1021
-                else:
-                    crc <<= 1
-                crc &= 0xFFFF
-        
-        if crc.to_bytes(2, 'big') == data[-2:]:
-            return True
-        return False
-
-    def __str__(self)->str:
-        return f"""Packet object with:
-        Type: {self.type.hex(" ")}
-        Timestamp: {self.timestamp.hex(" ")}
-        Data: {self.data.hex(" ")}
-        CRCPass: {self.crcpass}
-        """
 
 class Transfer:
 
